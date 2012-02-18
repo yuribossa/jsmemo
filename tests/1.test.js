83f110ca322f4ccb2c240eed92f3e488f93a4477
Takahashi Kiyoshi <yuribossa@gmail.com>
2012-02-18 14:45:05 +0900

    first commit

---
+++ tests/1.test.js
@@ -0,0 +1,257 @@
+
+exports.testNot = {
+"!false": function(test) {
+           test.ok(!false);
+           test.done();
+       },
+
+"!undefined": function(test) {
+           test.ok(!undefined);
+           test.done();
+       },
+
+"!0": function(test) {
+           test.ok(!0);
+           test.done();
+       },
+
+"!null": function(test) {
+           test.ok(!null);
+           test.done();
+       },
+
+"{}": function(test) {
+           test.ok({});
+           test.done();
+       },
+
+"[]": function(test) {
+           test.ok([]);
+           test.done();
+       },
+
+"!''": function(test) {
+           test.ok(!'');
+           test.done();
+       },
+
+}
+
+
+exports.testNaive = {
+"false != undefined": function(test) {
+           test.ok(false != undefined);
+           test.done();
+       },
+
+"false == 0": function(test) {
+           test.ok(false == 0);
+           test.done();
+       },
+
+"false != null": function(test) {
+           test.ok(false != null);
+           test.done();
+       },
+
+"false != {}": function(test) {
+           test.ok(false != {});
+           test.done();
+       },
+
+"false == []": function(test) {
+           test.ok(false == []);
+           test.done();
+       },
+
+"false == ''": function(test) {
+           test.ok(false == '');
+           test.done();
+       },
+
+"undefined != 0": function(test) {
+           test.ok(undefined != 0);
+           test.done();
+       },
+
+"undefined == null": function(test) {
+           test.ok(undefined == null);
+           test.done();
+       },
+
+"undefined != {}": function(test) {
+           test.ok(undefined != {});
+           test.done();
+       },
+
+"undefined != []": function(test) {
+            test.ok(undefined != []);
+            test.done();
+        },
+
+"undefined != ''": function(test) {
+            test.ok(undefined != '');
+            test.done();
+        },
+
+"0 != null": function(test) {
+           test.ok(0 != null);
+           test.done();
+       },
+
+"0 != {}": function(test) {
+           test.ok(0 != {});
+           test.done();
+       },
+
+"0 == []": function(test) {
+           test.ok(0 == []);
+           test.done();
+       },
+
+"0 == ''": function(test) {
+           test.ok(0 == '');
+           test.done();
+       },
+
+"null != {}": function(test) {
+           test.ok(null != {});
+           test.done();
+       },
+
+"null != []": function(test) {
+           test.ok(null != []);
+           test.done();
+       },
+
+"null != ''": function(test) {
+           test.ok(null != '');
+           test.done();
+       },
+
+"{} != []": function(test) {
+           test.ok({} != []);
+           test.done();
+       },
+
+"{} != ''": function(test) {
+           test.ok({} != '');
+           test.done();
+       },
+
+"[] == ''": function(test) {
+            test.ok([] == '');
+            test.done();
+        },
+}
+
+
+exports.testStrict = {
+"false !== undefined": function(test) {
+           test.ok(false !== undefined);
+           test.done();
+       },
+
+"false !== 0": function(test) {
+           test.ok(false !== 0);
+           test.done();
+       },
+
+"false !== null": function(test) {
+           test.ok(false !== null);
+           test.done();
+       },
+
+"false !== {}": function(test) {
+           test.ok(false !== {});
+           test.done();
+       },
+
+"false !== []": function(test) {
+           test.ok(false !== []);
+           test.done();
+       },
+
+"false !== ''": function(test) {
+           test.ok(false !== '');
+           test.done();
+       },
+
+"undefined !== 0": function(test) {
+           test.ok(undefined !== 0);
+           test.done();
+       },
+
+"undefined !== null": function(test) {
+           test.ok(undefined !== null);
+           test.done();
+       },
+
+"undefined !== {}": function(test) {
+           test.ok(undefined !== {});
+           test.done();
+       },
+
+"undefined !== []": function(test) {
+            test.ok(undefined !== []);
+            test.done();
+        },
+
+"undefined !== ''": function(test) {
+            test.ok(undefined !== '');
+            test.done();
+        },
+
+"0 !== null": function(test) {
+           test.ok(0 !== null);
+           test.done();
+       },
+
+"0 !== {}": function(test) {
+           test.ok(0 !== {});
+           test.done();
+       },
+
+"0 !== []": function(test) {
+           test.ok(0 !== []);
+           test.done();
+       },
+
+"0 !== ''": function(test) {
+           test.ok(0 !== '');
+           test.done();
+       },
+
+"null !== {}": function(test) {
+           test.ok(null !== {});
+           test.done();
+       },
+
+"null !== []": function(test) {
+           test.ok(null !== []);
+           test.done();
+       },
+
+"null !== ''": function(test) {
+           test.ok(null !== '');
+           test.done();
+       },
+
+"{} !== []": function(test) {
+           test.ok({} !== []);
+           test.done();
+       },
+
+"{} !== ''": function(test) {
+           test.ok({} !== '');
+           test.done();
+       },
+
+"[] !== ''": function(test) {
+            test.ok([] !== '');
+            test.done();
+        },
+}
+
+
+
---
+++ tests/2.test.coffee
@@ -0,0 +1,40 @@
+
+exports.testHatena = {
+    "false?": (test) ->
+        x = false
+        x = true if false?
+        test.ok x
+        test.done()
+
+    "not undefined?": (test) ->
+        x = false
+        x = true if undefined?
+        test.ok not x
+        test.done()
+
+    "0?": (test) ->
+        x = false
+        x = true if 0?
+        test.ok x
+        test.done()
+
+    "{}?": (test) ->
+        x = false
+        x = true if {}?
+        test.ok x
+        test.done()
+
+    "[]?": (test) ->
+        x = false
+        x = true if []?
+        test.ok x
+        test.done()
+
+    "''?": (test) ->
+        x = false
+        x = true if ''?
+        test.ok x
+        test.done()
+
+        }
+
